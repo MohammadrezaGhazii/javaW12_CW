8466c65d42f6b0e1c4a1ce4c2969dd0908b29d27
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-15 15:17:48 +0330

    add run method

--- src/main/java/org/example/MyThreadExt.java
+++ src/main/java/org/example/MyThreadExt.java
@@ -1,4 +1,12 @@
 package org.example;
 
-public class MyThreadExt {
+public class MyThreadExt extends Thread{
+    public MyThreadExt(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello Word - Extend");
+    }
 }
