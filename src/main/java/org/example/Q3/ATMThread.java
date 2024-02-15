d07732d478824fd3e12d3b02b5cfe7957bc9aeab
arianaaghajani <aghajaniariana@gmail.com>
2024-02-15 17:46:10 +0330

    add run method and constructor

--- src/main/java/org/example/Q3/ATMThread.java
+++ src/main/java/org/example/Q3/ATMThread.java
@@ -1,4 +1,18 @@
 package org.example.Q3;
 
-public class ATMThread {
+public class ATMThread implements Runnable{
+    Thread thread;
+    BankAccount bankAccount;
+    public ATMThread(String name, BankAccount bankAccount) {
+        thread=new Thread(this,name);
+        this.bankAccount=bankAccount;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100000; i++) {
+            bankAccount.deposit(10);
+        }
+
+    }
 }
