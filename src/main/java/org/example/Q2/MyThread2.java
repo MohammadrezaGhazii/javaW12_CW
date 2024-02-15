19f515ca425a083dc2d6a37b10cfd0b0e4134b48
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-15 17:07:01 +0330

    add MyThread2 method

--- src/main/java/org/example/Q2/MyThread2.java
+++ src/main/java/org/example/Q2/MyThread2.java
@@ -1,4 +1,16 @@
 package org.example.Q2;
 
-public class MyThread2 {
+public class MyThread2 implements Runnable {
+    String announce;
+    Thread thread;
+
+    public MyThread2(String announce, String name) {
+        this.announce = announce;
+        thread = new Thread(this,name);
+    }
+
+    @Override
+    public void run() {
+        System.out.println(announce);
+    }
 }
