edfe86ee046ba086d1ec6c3ab79a263b3263a41f
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-15 16:21:46 +0330

    final code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("main thread - 1");
+        MyThreadExt myThreadExt = new MyThreadExt("Child1");
+//        myThreadExt.setPriority(Thread.MAX_PRIORITY);
+        myThreadExt.setDaemon(true);
+        myThreadExt.start();
+
+        System.out.println("main thread - 2");
+        MyThreadImpl myThreadImpl = new MyThreadImpl("Child2") ;
+        Thread thread = new Thread(myThreadImpl);
+//        thread.setPriority(Thread.MIN_PRIORITY);
+        thread.start();
+
+        try {
+            myThreadExt.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("End of program");
     }
 }
