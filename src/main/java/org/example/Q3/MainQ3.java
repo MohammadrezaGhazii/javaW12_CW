bf29338c6bdf8b4056617c0b1c183f7a807d93f0
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-20 19:35:34 +0330

    fix bug

--- src/main/java/org/example/Q3/MainQ3.java
+++ src/main/java/org/example/Q3/MainQ3.java
@@ -2,20 +2,16 @@ package org.example.Q3;
 
 public class MainQ3 {
     public static void main(String[] args) throws InterruptedException {
-        BankAccount account1=new BankAccount();
-        ATMThread atmThread1=new ATMThread("account1",account1);
+        BankAccount account=new BankAccount();
+        ATMThread atmThread1=new ATMThread("account1",account);
         Thread tAccount1=new Thread(atmThread1);
-        BankAccount account2=new BankAccount();
-        ATMThread atmThread2=new ATMThread("account2",account2);
+        ATMThread atmThread2=new ATMThread("account2",account);
         Thread tAccount2=new Thread(atmThread2);
-        BankAccount account3=new BankAccount();
-        ATMThread atmThread3=new ATMThread("account3",account3);
+        ATMThread atmThread3=new ATMThread("account3",account);
         Thread tAccount3=new Thread(atmThread3);
-        BankAccount account4=new BankAccount();
-        ATMThread atmThread4=new ATMThread("account4",account4);
+        ATMThread atmThread4=new ATMThread("account4",account);
         Thread tAccount4=new Thread(atmThread4);
-        BankAccount account5=new BankAccount();
-        ATMThread atmThread5=new ATMThread("account5",account5);
+        ATMThread atmThread5=new ATMThread("account5",account);
         Thread tAccount5=new Thread(atmThread5);
 
         tAccount1.start();
@@ -30,10 +26,6 @@ public class MainQ3 {
         tAccount4.join();
         tAccount5.join();
 
-        System.out.println("balance1= " +account1.getBalance());
-        System.out.println("balance2= " +account2.getBalance());
-        System.out.println("balance3= " +account3.getBalance());
-        System.out.println("balance4= " +account4.getBalance());
-        System.out.println("balance5= " +account5.getBalance());
+        System.out.println("balance1= " +account.getBalance());
     }
 }
