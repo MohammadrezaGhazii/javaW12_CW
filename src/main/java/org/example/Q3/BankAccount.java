486774970e784580f18238790a876ec40be2546b
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-02-20 19:57:56 +0330

    edit

--- src/main/java/org/example/Q3/BankAccount.java
+++ src/main/java/org/example/Q3/BankAccount.java
@@ -5,20 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class BankAccount {
-   private int balance;
+   private AtomicInteger balance = new AtomicInteger(0);
 
-   public void deposit(int amount){
-      this.balance += amount;
-      System.out.println("new balance is :" + this.balance);
+    public void deposit(int amount){
+      this.balance.addAndGet(amount);
    }
    public void withdraw(int amount){
-      if (balance >= amount){
-         this.balance -=amount;
+      if (balance.get() >= amount){
+         this.balance =new AtomicInteger(this.balance.addAndGet(-amount));
       }
       else {
          System.out.println("your balance is not enough ");
